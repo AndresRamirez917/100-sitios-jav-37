992a82a0fdac1d307165fd8bbb7408ec74362178
AndresRamirez917 <andresmintic2021@gmail.com>
2024-10-02 16:47:53 -0500

    first commit

--- js/index.js
+++ js/index.js
@@ -2,12 +2,13 @@ async function getData(){
     const result = await fetch('https://randomuser.me/api?results=3');
     const fakeUser = await result.json()
     console.log(fakeUser)
+    const arr = [1,2,3,4]
     fakeUser.results.forEach(element => {
         for(i = 0; i < 1; i++){
-            if(element.id = "ella.kurpita@example.com"){
+            //for(j = 1; j < arr.length; j++){
                 const box = document.createRange().createContextualFragment(`
                     
-                <div class="box box-1">
+                <div class="box box-${i}">
                     <img src="${element.picture.large}" alt="">
                     <h3>${element.name.first} ${element.name.last}</h3>
                     <h3>Age: ${element.dob.age}</h3>
@@ -19,8 +20,47 @@ async function getData(){
                     `)
                     const services_row = document.querySelector('.services-row');
                     services_row.append(box)
-            }
+            //}
         }
     });
 }
+
+const btn_validar = document.getElementById('btn-validar').onclick = (e) => {
+    e.preventDefault()
+    const nombre = document.getElementById('nombre');
+    const email = document.getElementById('email');
+    const mensaje = document.getElementById('mensaje');
+    const arr = [];
+    const messageArr = ["Nombre", "Email", "Mensaje"];
+    arr.push(nombre, email, mensaje);
+    for(i = 0; i < arr.length; i++){
+        if(arr[i].value == ""){
+            swal({
+                title: `El campo ${messageArr[i]} no puede estar vacío`,
+                icon: "error",
+                 })
+                 return false;
+        }
+    }
+    if(!emailValido(email)){
+        swal({
+            title: `El campo ${messageArr[1]} no tiene el formato correcto`,
+            icon: "error",
+             })
+             return false;
+    }
+    swal({
+        title: `Datos enviados satisfactoriamente`,
+        icon: "success",
+         })
+         nombre.value = "";
+         email.value = "";
+         mensaje.value = "";
+         return true;
+         
+        }
+        const emailValido = (email) => {
+            return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email.value)
+        }
+
 getData()
